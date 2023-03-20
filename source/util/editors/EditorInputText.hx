ee8327c79b0e0d31926ffe65ddfbe9bee443c418
Starmapo <fabian.xavier7@gmail.com>
2023-03-19 21:31:37 -0400

    input text

--- source/util/editors/EditorInputText.hx
+++ source/util/editors/EditorInputText.hx
@@ -8,37 +8,44 @@ class EditorInputText extends FlxText
 	public function new(x:Float = 0, y:Float = 0, fieldWidth:Float = 0, ?text:String, size:Int = 8, embeddedFont:Bool = true)
 	{
 		super(x, y, fieldWidth, text, size, embeddedFont);
+		scrollFactor.set();
 		textField.selectable = true;
 		textField.type = INPUT;
 		FlxG.game.addChild(textField);
+		FlxG.signals.gameResized.add(onGameResized);
 	}
 
 	override function draw()
 	{
-		super.draw();
-
-		var position = getGlobalPosition(null, camera);
-
-		textField.x = FlxG.game.x + position.x - offset.x;
-		textField.y = FlxG.game.y + position.y - offset.y;
-
-		#if !web
-		textField.x -= 1;
-		textField.y -= 1;
-		#end
-
-		textField.scaleX = camera.totalScaleX * scale.x;
-		textField.scaleY = camera.totalScaleY * scale.y;
-
-		textField.x -= 0.5 * camera.width * (camera.scaleX - 1) * FlxG.scaleMode.scale.x;
-		textField.y -= 0.5 * camera.height * (camera.scaleY - 1) * FlxG.scaleMode.scale.y;
-
-		position.put();
+		updateTextField();
 	}
 
 	override function destroy()
 	{
 		FlxG.game.removeChild(textField);
+		FlxG.signals.gameResized.remove(onGameResized);
 		super.destroy();
+	}
+
+	function updateTextField()
+	{
+		if (textField == null)
+			return;
+
+		textField.scaleX = camera.totalScaleX * scale.x;
+		textField.scaleY = camera.totalScaleY * scale.y;
+
+		textField.x = (x - offset.x) * textField.scaleX;
+		textField.y = (y - offset.y) * textField.scaleY;
+
+		#if !web
+		textField.x -= 1;
+		textField.y -= 1;
+		#end
+	}
+
+	function onGameResized(_, _)
+	{
+		updateTextField();
 	}
 }
